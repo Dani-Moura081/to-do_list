e0aa6593ea61300b3a17b3ab2eebd47c53d45062
dani-moura081 <dani.fe.olav.rodrigues@gmail.com>
2024-07-18 16:29:28 -0300

    primeiro commit da lista de tarefas. Tá simples por enquanto, porém pretendo melhorar no futuro.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import { useState } from 'react';
 
 function App() {
+  const [itemList, setItemList] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  function handleAddItem() {
+    const textWithoutSpace = inputText.trim(); // tira os espaços em branco inicio e fim do texto
+    if (textWithoutSpace === "") return; // impede de add uma tarefa em branco
+
+    let newItem = {
+      id: Date.now(),
+      itemList: textWithoutSpace,
+    };
+
+    setItemList([...itemList, newItem]);
+    setInputText("");
+  }
+
+  function handleDeleteItem(item) {
+    setItemList((prevList) => prevList.filter((list) => list.id !== item.id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button className='AddItemButton' onClick={handleAddItem}>Add</button>
+      </div>
+
+      <ul className='list'>
+        {itemList.map((item) => (
+          <li className='item' key={item.id}>
+            {item.itemList}
+            <button
+              className="DeleteItemButton"
+              onClick={() => handleDeleteItem(item)}
+            >
+              X
+            </button>{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
